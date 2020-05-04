cc19c92e100bddd6db099299bf53218ac02542d8
Tiago Matos <tiago@tiagomatos.com>
2020-05-04 17:23:53 +1000

    Enhance buttons

--- src/App.vue
+++ src/App.vue
@@ -4,27 +4,9 @@
         class="container mx-auto my-5"
     >
         <div class="grid gap-8">
-            <div>
-                <TwLabel>Badge</TwLabel>
-                <div class="grid gap-4">
-                    <div
-                        v-for="(badge, index) in badges"
-                        :key="index"
-                    >
-                        <TwBadge
-                            v-for="(b, idx) in badge"
-                            :key="idx"
-                            :theme="b.theme"
-                            :color="b.color"
-                            :pill="b.pill"
-                            :size="b.size"
-                            :class="b.class"
-                        >
-                            Badge
-                        </TwBadge>
-                    </div>
-                </div>
-            </div>
+            <Badges />
+
+            <Buttons />
 
             <div>
                 <TwLabel>Dropdown</TwLabel>
@@ -71,171 +53,14 @@
 <script>
     import TwDropdown from './components/TwDropdown';
     import TwLabel from '@/components/TwLabel';
-    import TwBadge from '@/components/TwBadge';
+    import Buttons from '@/Buttons';
+    import Badges from '@/Badges';
 
     export default {
         name: "App",
-        components: { TwBadge, TwLabel, TwDropdown },
+        components: { Badges, TwLabel, TwDropdown, Buttons },
         data() {
             return {
-                badges: [
-                    [
-                        { size: 'sm', theme: 'light', color: 'gray', pill: false, class: null },
-                        { size: 'sm', theme: 'light', color: 'cool-gray', pill: false, class: 'mx-1' },
-                        { size: 'sm', theme: 'light', color: 'red', pill: false, class: 'mx-1' },
-                        { size: 'sm', theme: 'light', color: 'orange', pill: false, class: 'mx-1' },
-                        { size: 'sm', theme: 'light', color: 'yellow', pill: false, class: 'mx-1' },
-                        { size: 'sm', theme: 'light', color: 'green', pill: false, class: 'mx-1' },
-                        { size: 'sm', theme: 'light', color: 'teal', pill: false, class: 'mx-1' },
-                        { size: 'sm', theme: 'light', color: 'indigo', pill: false, class: 'mx-1' },
-                        { size: 'sm', theme: 'light', color: 'blue', pill: false, class: 'mx-1' },
-                        { size: 'sm', theme: 'light', color: 'purple', pill: false, class: 'mx-1' },
-                        { size: 'sm', theme: 'light', color: 'pink', pill: false, class: 'mx-1' },
-                    ],
-                    [
-                        { size: 'sm', theme: 'dark', color: 'gray', pill: false, class: null },
-                        { size: 'sm', theme: 'dark', color: 'cool-gray', pill: false, class: 'mx-1' },
-                        { size: 'sm', theme: 'dark', color: 'red', pill: false, class: 'mx-1' },
-                        { size: 'sm', theme: 'dark', color: 'orange', pill: false, class: 'mx-1' },
-                        { size: 'sm', theme: 'dark', color: 'yellow', pill: false, class: 'mx-1' },
-                        { size: 'sm', theme: 'dark', color: 'green', pill: false, class: 'mx-1' },
-                        { size: 'sm', theme: 'dark', color: 'teal', pill: false, class: 'mx-1' },
-                        { size: 'sm', theme: 'dark', color: 'indigo', pill: false, class: 'mx-1' },
-                        { size: 'sm', theme: 'dark', color: 'blue', pill: false, class: 'mx-1' },
-                        { size: 'sm', theme: 'dark', color: 'purple', pill: false, class: 'mx-1' },
-                        { size: 'sm', theme: 'dark', color: 'pink', pill: false, class: 'mx-1' },
-                    ],
-                    [
-                        { size: 'sm', theme: 'light', color: 'gray', pill: true, class: null },
-                        { size: 'sm', theme: 'light', color: 'cool-gray', pill: true, class: 'mx-1' },
-                        { size: 'sm', theme: 'light', color: 'red', pill: true, class: 'mx-1' },
-                        { size: 'sm', theme: 'light', color: 'orange', pill: true, class: 'mx-1' },
-                        { size: 'sm', theme: 'light', color: 'yellow', pill: true, class: 'mx-1' },
-                        { size: 'sm', theme: 'light', color: 'green', pill: true, class: 'mx-1' },
-                        { size: 'sm', theme: 'light', color: 'teal', pill: true, class: 'mx-1' },
-                        { size: 'sm', theme: 'light', color: 'indigo', pill: true, class: 'mx-1' },
-                        { size: 'sm', theme: 'light', color: 'blue', pill: true, class: 'mx-1' },
-                        { size: 'sm', theme: 'light', color: 'purple', pill: true, class: 'mx-1' },
-                        { size: 'sm', theme: 'light', color: 'pink', pill: true, class: 'mx-1' },
-                    ],
-                    [
-                        { size: 'sm', theme: 'dark', color: 'gray', pill: true, class: null },
-                        { size: 'sm', theme: 'dark', color: 'cool-gray', pill: true, class: 'mx-1' },
-                        { size: 'sm', theme: 'dark', color: 'red', pill: true, class: 'mx-1' },
-                        { size: 'sm', theme: 'dark', color: 'orange', pill: true, class: 'mx-1' },
-                        { size: 'sm', theme: 'dark', color: 'yellow', pill: true, class: 'mx-1' },
-                        { size: 'sm', theme: 'dark', color: 'green', pill: true, class: 'mx-1' },
-                        { size: 'sm', theme: 'dark', color: 'teal', pill: true, class: 'mx-1' },
-                        { size: 'sm', theme: 'dark', color: 'indigo', pill: true, class: 'mx-1' },
-                        { size: 'sm', theme: 'dark', color: 'blue', pill: true, class: 'mx-1' },
-                        { size: 'sm', theme: 'dark', color: 'purple', pill: true, class: 'mx-1' },
-                        { size: 'sm', theme: 'dark', color: 'pink', pill: true, class: 'mx-1' },
-                    ],
-                    [
-                        { size: 'md', theme: 'light', color: 'gray', pill: false, class: null },
-                        { size: 'md', theme: 'light', color: 'cool-gray', pill: false, class: 'mx-1' },
-                        { size: 'md', theme: 'light', color: 'red', pill: false, class: 'mx-1' },
-                        { size: 'md', theme: 'light', color: 'orange', pill: false, class: 'mx-1' },
-                        { size: 'md', theme: 'light', color: 'yellow', pill: false, class: 'mx-1' },
-                        { size: 'md', theme: 'light', color: 'green', pill: false, class: 'mx-1' },
-                        { size: 'md', theme: 'light', color: 'teal', pill: false, class: 'mx-1' },
-                        { size: 'md', theme: 'light', color: 'indigo', pill: false, class: 'mx-1' },
-                        { size: 'md', theme: 'light', color: 'blue', pill: false, class: 'mx-1' },
-                        { size: 'md', theme: 'light', color: 'purple', pill: false, class: 'mx-1' },
-                        { size: 'md', theme: 'light', color: 'pink', pill: false, class: 'mx-1' },
-                    ],
-                    [
-                        { size: 'md', theme: 'dark', color: 'gray', pill: false, class: null },
-                        { size: 'md', theme: 'dark', color: 'cool-gray', pill: false, class: 'mx-1' },
-                        { size: 'md', theme: 'dark', color: 'red', pill: false, class: 'mx-1' },
-                        { size: 'md', theme: 'dark', color: 'orange', pill: false, class: 'mx-1' },
-                        { size: 'md', theme: 'dark', color: 'yellow', pill: false, class: 'mx-1' },
-                        { size: 'md', theme: 'dark', color: 'green', pill: false, class: 'mx-1' },
-                        { size: 'md', theme: 'dark', color: 'teal', pill: false, class: 'mx-1' },
-                        { size: 'md', theme: 'dark', color: 'indigo', pill: false, class: 'mx-1' },
-                        { size: 'md', theme: 'dark', color: 'blue', pill: false, class: 'mx-1' },
-                        { size: 'md', theme: 'dark', color: 'purple', pill: false, class: 'mx-1' },
-                        { size: 'md', theme: 'dark', color: 'pink', pill: false, class: 'mx-1' },
-                    ],
-                    [
-                        { size: 'md', theme: 'light', color: 'gray', pill: true, class: null },
-                        { size: 'md', theme: 'light', color: 'cool-gray', pill: true, class: 'mx-1' },
-                        { size: 'md', theme: 'light', color: 'red', pill: true, class: 'mx-1' },
-                        { size: 'md', theme: 'light', color: 'orange', pill: true, class: 'mx-1' },
-                        { size: 'md', theme: 'light', color: 'yellow', pill: true, class: 'mx-1' },
-                        { size: 'md', theme: 'light', color: 'green', pill: true, class: 'mx-1' },
-                        { size: 'md', theme: 'light', color: 'teal', pill: true, class: 'mx-1' },
-                        { size: 'md', theme: 'light', color: 'indigo', pill: true, class: 'mx-1' },
-                        { size: 'md', theme: 'light', color: 'blue', pill: true, class: 'mx-1' },
-                        { size: 'md', theme: 'light', color: 'purple', pill: true, class: 'mx-1' },
-                        { size: 'md', theme: 'light', color: 'pink', pill: true, class: 'mx-1' },
-                    ],
-                    [
-                        { size: 'md', theme: 'dark', color: 'gray', pill: true, class: null },
-                        { size: 'md', theme: 'dark', color: 'cool-gray', pill: true, class: 'mx-1' },
-                        { size: 'md', theme: 'dark', color: 'red', pill: true, class: 'mx-1' },
-                        { size: 'md', theme: 'dark', color: 'orange', pill: true, class: 'mx-1' },
-                        { size: 'md', theme: 'dark', color: 'yellow', pill: true, class: 'mx-1' },
-                        { size: 'md', theme: 'dark', color: 'green', pill: true, class: 'mx-1' },
-                        { size: 'md', theme: 'dark', color: 'teal', pill: true, class: 'mx-1' },
-                        { size: 'md', theme: 'dark', color: 'indigo', pill: true, class: 'mx-1' },
-                        { size: 'md', theme: 'dark', color: 'blue', pill: true, class: 'mx-1' },
-                        { size: 'md', theme: 'dark', color: 'purple', pill: true, class: 'mx-1' },
-                        { size: 'md', theme: 'dark', color: 'pink', pill: true, class: 'mx-1' },
-                    ],
-                    [
-                        { size: 'lg', theme: 'light', color: 'gray', pill: false, class: null },
-                        { size: 'lg', theme: 'light', color: 'cool-gray', pill: false, class: 'mx-1' },
-                        { size: 'lg', theme: 'light', color: 'red', pill: false, class: 'mx-1' },
-                        { size: 'lg', theme: 'light',  color: 'orange', pill: false, class: 'mx-1' },
-                        { size: 'lg', theme: 'light',  color: 'yellow', pill: false, class: 'mx-1' },
-                        { size: 'lg', theme: 'light',  color: 'green', pill: false, class: 'mx-1' },
-                        { size: 'lg', theme: 'light',  color: 'teal', pill: false, class: 'mx-1' },
-                        { size: 'lg', theme: 'light',  color: 'indigo', pill: false, class: 'mx-1' },
-                        { size: 'lg', theme: 'light',  color: 'blue', pill: false, class: 'mx-1' },
-                        { size: 'lg', theme: 'light',  color: 'purple', pill: false, class: 'mx-1' },
-                        { size: 'lg', theme: 'light',  color: 'pink', pill: false, class: 'mx-1' },
-                    ],
-                    [
-                        { size: 'lg', theme: 'dark', color: 'gray', pill: false, class: null },
-                        { size: 'lg', theme: 'dark', color: 'cool-gray', pill: false, class: 'mx-1' },
-                        { size: 'lg', theme: 'dark', color: 'red', pill: false, class: 'mx-1' },
-                        { size: 'lg', theme: 'dark',  color: 'orange', pill: false, class: 'mx-1' },
-                        { size: 'lg', theme: 'dark',  color: 'yellow', pill: false, class: 'mx-1' },
-                        { size: 'lg', theme: 'dark',  color: 'green', pill: false, class: 'mx-1' },
-                        { size: 'lg', theme: 'dark',  color: 'teal', pill: false, class: 'mx-1' },
-                        { size: 'lg', theme: 'dark',  color: 'indigo', pill: false, class: 'mx-1' },
-                        { size: 'lg', theme: 'dark',  color: 'blue', pill: false, class: 'mx-1' },
-                        { size: 'lg', theme: 'dark',  color: 'purple', pill: false, class: 'mx-1' },
-                        { size: 'lg', theme: 'dark',  color: 'pink', pill: false, class: 'mx-1' },
-                    ],
-                    [
-                        { size: 'lg', theme: 'light',  color: 'gray', pill: true, class: null },
-                        { size: 'lg', theme: 'light',  color: 'cool-gray', pill: true, class: 'mx-1' },
-                        { size: 'lg', theme: 'light',  color: 'red', pill: true, class: 'mx-1' },
-                        { size: 'lg', theme: 'light',  color: 'orange', pill: true, class: 'mx-1' },
-                        { size: 'lg', theme: 'light',  color: 'yellow', pill: true, class: 'mx-1' },
-                        { size: 'lg', theme: 'light',  color: 'green', pill: true, class: 'mx-1' },
-                        { size: 'lg', theme: 'light',  color: 'teal', pill: true, class: 'mx-1' },
-                        { size: 'lg', theme: 'light',  color: 'indigo', pill: true, class: 'mx-1' },
-                        { size: 'lg', theme: 'light',  color: 'blue', pill: true, class: 'mx-1' },
-                        { size: 'lg', theme: 'light',  color: 'purple', pill: true, class: 'mx-1' },
-                        { size: 'lg', theme: 'light',  color: 'pink', pill: true, class: 'mx-1' },
-                    ],
-                    [
-                        { size: 'lg', theme: 'dark',  color: 'gray', pill: true, class: null },
-                        { size: 'lg', theme: 'dark',  color: 'cool-gray', pill: true, class: 'mx-1' },
-                        { size: 'lg', theme: 'dark',  color: 'red', pill: true, class: 'mx-1' },
-                        { size: 'lg', theme: 'dark',  color: 'orange', pill: true, class: 'mx-1' },
-                        { size: 'lg', theme: 'dark',  color: 'yellow', pill: true, class: 'mx-1' },
-                        { size: 'lg', theme: 'dark',  color: 'green', pill: true, class: 'mx-1' },
-                        { size: 'lg', theme: 'dark',  color: 'teal', pill: true, class: 'mx-1' },
-                        { size: 'lg', theme: 'dark',  color: 'indigo', pill: true, class: 'mx-1' },
-                        { size: 'lg', theme: 'dark',  color: 'blue', pill: true, class: 'mx-1' },
-                        { size: 'lg', theme: 'dark',  color: 'purple', pill: true, class: 'mx-1' },
-                        { size: 'lg', theme: 'dark',  color: 'pink', pill: true, class: 'mx-1' },
-                    ],
-                ],
                 options: [
                     { id: 1, label: 'Option 1' },
                     { id: 2, label: 'Option 2' },
--- src/components/TwButton.vue
+++ src/components/TwButton.vue
@@ -33,6 +33,12 @@
 
             theme: {
                 type: String,
+                default: 'light',
+                validator: value => ['light', 'dark', 'darker'].includes(value),
+            },
+
+            color: {
+                type: String,
                 default: 'gray',
                 validator: value => TW_COLORS.includes(value),
             },
@@ -46,6 +52,16 @@
                 type: String,
                 default: 'md',
                 validator: value => ['xs', 'sm', 'md', 'lg', 'xl'].includes(value),
+            },
+
+            pill: {
+                type: Boolean,
+                default: false,
+            },
+
+            outline: {
+                type: Boolean,
+                default: false,
             },
 
             shadow: {
@@ -69,7 +85,7 @@
                 default: false,
             },
 
-            clean: {
+            naked: {
                 type: Boolean,
                 default: false,
             },
@@ -86,65 +102,86 @@
                 }
 
                 return [
-                    'inline-flex items-center justify-center font-medium rounded transition ease-in-out duration-150',
+                    'inline-flex items-center justify-center font-medium transition ease-in-out duration-150',
                     this.getTextColorClass,
                     this.getBgColorClass,
                     this.getBlockClass,
                     this.getDisabledClass,
                     this.getTextSizeClass,
                     this.getPaddingClass,
-                    this.getBorderClass,
+                    this.getBorderColorClass,
+                    this.getBorderRoundedClass,
                     this.getShadowClass,
                 ];
             },
 
             getTextSizeClass() {
-                switch (this.size) {
-                    case 'xs':
-                        return 'text-xs leading-4';
-                    case 'sm':
-                        return 'text-sm leading-4';
-                    case 'md':
-                        return 'text-sm leading-5';
-                    case 'lg':
-                        return 'text-base leading-6';
-                    case 'xl':
-                        return 'text-base leading-6';
-                    default:
-                        return '';
-                }
+                const sizes = {
+                    xs: 'text-xs leading-4',
+                    sm: 'text-sm leading-4',
+                    md: 'text-sm leading-5',
+                    lg: 'text-base leading-6',
+                    xl: 'text-base leading-6',
+                };
+
+                return sizes[this.size];
             },
 
             getTextColorClass() {
-                if (this.clean) {
-                    return `text-${this.theme}-800`;
+                if (this.naked) {
+                    return `text-${this.color}-800`;
                 }
 
-                return `text-${this.theme}-100`;
+                const themes = {
+                    light: `${this.outline ? `text-${this.color}-400 hover:text-${this.color}-500 active:text-${this.color}-400` : `text-${this.color}-800 hover:text-${this.color}-900` }`,
+                    dark: `${this.outline ? `text-${this.color}-400 hover:text-${this.color}-500 active:text-${this.color}-500` : `text-${this.color}-800 hover:text-${this.color}-900` }`,
+                    darker: `${this.outline ? `text-${this.color}-500 hover:text-${this.color}-600 active:text-${this.color}-600` : `text-${this.color}-50 hover:text-${this.color}-100` }`,
+                };
+
+                return themes[this.theme];
             },
 
             getBgColorClass() {
-                if (this.clean) { return false; }
-                return `bg-${this.theme}-600 ${!this.isBusy ? `hover:bg-${this.theme}-500 active:bg-${this.theme}-700 focus:shadow-outline-${this.theme}` : ''}`;
+                if (this.naked || this.outline) { return false; }
+
+                const themes = {
+                    light: `bg-${this.color}-100 hover:bg-${this.color}-200 active:bg-${this.color}-200`,
+                    dark: `bg-${this.color}-300 hover:bg-${this.color}-400 active:bg-${this.color}-400`,
+                    darker: `bg-${this.color}-500 hover:bg-${this.color}-600 active:bg-${this.color}-600`,
+                };
+
+                return themes[this.theme];
+            },
+
+            getBorderColorClass() {
+                const themes = {
+                    light:
+                        `border ${this.outline ? `border-${this.color}-300 hover:border-${this.color}-400 active:border-${this.color}-400` : 'border-transparent'} focus:shadow-outline-${this.color} focus:outline-none`,
+                    dark:
+                        `border ${this.outline ? `border-${this.color}-400 hover:border-${this.color}-500 active:border-${this.color}-500` : 'border-transparent'} focus:shadow-outline-${this.color} focus:outline-none`,
+                    darker:
+                        `border ${this.outline ? `border-${this.color}-500 hover:border-${this.color}-600 active:border-${this.color}-600` : 'border-transparent'} focus:shadow-outline-${this.color} focus:outline-none`,
+                };
+
+                return themes[this.theme];
+            },
+
+            getBorderRoundedClass() {
+                return this.pill ? 'rounded-full' : 'rounded';
             },
 
             getPaddingClass() {
-                if (this.clean) { return ''; }
+                if (this.naked) { return ''; }
 
-                switch (this.size) {
-                    case 'xs':
-                        return 'px-2.5 py-1.5';
-                    case 'sm':
-                        return 'px-3 py-2';
-                    case 'md':
-                        return 'px-4 py-2';
-                    case 'lg':
-                        return 'px-4 py-2';
-                    case 'xl':
-                        return 'px-6 py-3';
-                    default:
-                        return '';
-                }
+                const sizes = {
+                    xs: 'px-2.5 py-1.5',
+                    sm: 'px-3 py-2',
+                    md: 'px-4 py-2',
+                    lg: 'px-4 py-2',
+                    xl: 'px-6 py-3',
+                };
+
+                return sizes[this.size];
             },
 
             getBlockClass() {
@@ -153,10 +190,6 @@
 
             getDisabledClass() {
                 return this.isBusy ? 'opacity-75' : '';
-            },
-
-            getBorderClass() {
-                return 'border border-transparent focus:outline-none';
             },
 
             getShadowClass() {
