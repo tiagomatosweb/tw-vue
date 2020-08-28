58366e28519143f70db64fba8c014d61ff4db996
elinardo10 <elinardosilva@gmail.com>
2020-08-28 00:44:44 -0300

    workin in togle3

--- src/TwToggles.vue
+++ src/TwToggles.vue
@@ -4,7 +4,9 @@
             TwToggle
         </h3>
         <div class="grid gap-4">
-            <TwToggle /> 
+            <TwToggle 
+                simple
+            /> 
      
             <TwToggle 
                 short
@@ -25,38 +27,13 @@
 
         data() {
             return {
-                isActive: false,
             };
         },
 
         computed: {
-            classList() {
-                return [
-                    'relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:shadow-outline',
-                    this.getActiveClass,
-                ];
-            },
-
-            getActiveClass() {
-                return this.isActive ? 'bg-indigo-600' : 'bg-gray-200';
-            },
-
-            bodyClassList(){
-                return [
-                    this.getBodyActiveClass,
-                    'inline-block h-5 w-5 rounded-full bg-white shadow transform transition ease-in-out duration-200',
-                ];
-            },
-
-            getBodyActiveClass(){
-                return this.isActive ? 'translate-x-5' : 'translate-x-0';
-            },
         },
 
         methods: {
-            changeActive(){
-                this.isActive = !this.isActive;
-            },
         },
     };
 </script>
--- src/components/TwToggle.vue
+++ src/components/TwToggle.vue
@@ -2,15 +2,14 @@
     <span
         role="checkbox"
         tabindex="0"
-        aria-hidden="true"
+        :aria-hidden="simple"
         :aria-checked="isActive"
         :class="classList"
         @click="changeActive()"
     >
         <!-- On: "bg-indigo-600", Off: "bg-gray-200" -->
         <span
-            aria-hidden="true"
-            tabindex="0"
+            :aria-hidden="!simple"
             :class="classList"
         >
             <!-- On: "translate-x-5", Off: "translate-x-0" -->
@@ -35,6 +34,11 @@
                 type: Boolean,
                 default: false,
             },
+
+            simple: {
+                type: Boolean,
+                default: false,
+            },
         },
 
         data() {
@@ -46,9 +50,11 @@
         computed: {
             classList() {
                 return [
+                    this.getDefaultClass,
                     this.getSimpleClass,
-                    this.getShadowClass,
+                    this.getShondowClass,
                     this.getActiveClass,
+                    this.getShortClass,
                 ];
             },
 
@@ -58,29 +64,47 @@
 
             bodyClassList(){
                 return [
+                    this.getDefaultBodyClass,
+                    this.getDefaultActive,
                     this.getBodyActiveClass,
-                    this.getShondowBodyClass,
+                    this.getShordBodyClass,
                     this.getSimpleBodyClass,
                 ];
             },
 
-            getBodyActiveClass(){
-                return this.isActive ? 'translate-x-5' : 'translate-x-0';
+            getDefaultClass() { 
+                return !this.simple && !this.short ? 'bg-gray-200 relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:shadow-outline' : '';
+            },
+
+            getDefaultBodyClass() { 
+                return !this.simple && !this.short ? 'inline-block h-5 w-5 rounded-full bg-white shadow transform transition ease-in-out duration-200' : '';
+            },
+
+            getDefaultActive(){
+                return this.isActive;
             },
 
             getSimpleClass() {
-                return !this.short ?  'bg-gray-200 relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:shadow-outline' : 'bg-gray-200 absolute h-4 w-9 mx-auto rounded-full transition-colors ease-in-out duration-200';
+                return this.simple ? 'bg-gray-200 relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:shadow-outline' : '';
             },
-
-            getShadowClass() {
+            
+            getBodyActiveClass(){
+                return this.isActive ? 'translate-x-5' : 'translate-x-0';
+            },
+          
+            getShondowClass() {
                 return this.short ? 'group relative inline-flex items-center justify-center flex-shrink-0 h-5 w-10 cursor-pointer focus:outline-none' : '';
             },
 
-            getSimpleBodyClass() {
-                return !this.short ? 'inline-block h-5 w-5 rounded-full bg-white shadow transform transition ease-in-out duration-200' : '';
+            getShortClass() {
+                return this.short ? 'bg-gray-200 absolute h-4 w-9 mx-auto rounded-full transition-colors ease-in-out duration-200' : '';
             },
 
-            getShondowBodyClass() {
+            getSimpleBodyClass() {
+                return this.simple ? 'inline-block h-5 w-5 rounded-full bg-white shadow transform transition ease-in-out duration-200' : '';
+            },
+
+            getShordBodyClass() {
                 return this.short ? 'absolute left-0 inline-block h-5 w-5 border border-gray-200 rounded-full bg-white shadow transform group-focus:shadow-outline group-focus:border-blue-300 transition-transform ease-in-out duration-200' : '';
             },
 
