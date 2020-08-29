bc1c73df7fe6db836272de25d8866b9ac989f1fb
elinardo10 <elinardosilva@gmail.com>
2020-08-29 02:19:56 -0300

    workin in togle4

--- src/TwToggles.vue
+++ src/TwToggles.vue
@@ -10,7 +10,11 @@
      
             <TwToggle 
                 short
-            />     
+            />
+
+            <TwToggle 
+                icon
+            />      
         </div>
     </div>
 </template>
