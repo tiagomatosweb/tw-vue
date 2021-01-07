cffade71c3e43baec13746c1402e126bf83dcfd6
Tiago Matos <tiago@tiagomatos.com>
2021-01-07 11:04:57 +1100

    New assets

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,16 @@
 <template>
     <div class="about">
         <h1>This is an about page</h1>
+        <Paginations />
     </div>
 </template>
+
+<script>
+    import Paginations from '@/Paginations';
+
+    export default {
+        components: {
+            Paginations,
+        },
+    };
+</script>
