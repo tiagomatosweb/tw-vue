dbc943953ef6e93893dc17bdda53b790406261ce
Tiago Matos <tiago@tiagomatos.com>
2021-10-05 11:32:01 +1100

    build

--- src/components/form-input/config.js
+++ src/components/form-input/config.js
@@ -1,12 +1,12 @@
 export default {
     base: 'block w-full transition ease-in-out duration-150',
     variants: {
-        default: 'text-gray-800 placeholder-gray-300 border-gray-300 focus:border-blue-300 focus:ring focus:ring-opacity-50 focus:ring-blue-200 shadow-sm ',
-        naked: 'text-gray-800 placeholder-gray-300 border-transparent bg-transparent focus:outline-none focus:border-transparent focus:ring-0',
-        success: 'text-gray-800 placeholder-gray-300 border-green-500 focus:border-green-500 focus:ring focus:ring-opacity-50 focus:ring-green-200 shadow-sm ',
-        danger: 'text-gray-800 placeholder-gray-300 border-red-500 focus:border-red-500 focus:ring focus:ring-opacity-50 focus:ring-red-200 shadow-sm ',
-        warning: 'text-gray-800 placeholder-gray-300 border-yellow-500 focus:border-yellow-500 focus:ring focus:ring-opacity-50 focus:ring-yellow-200 shadow-sm ',
-        info: 'text-gray-800 placeholder-gray-300 border-cyan-500 focus:border-cyan-500 focus:ring focus:ring-opacity-50 focus:ring-cyan-200 shadow-sm ',
+        default: 'text-gray-800 bg-white placeholder-gray-300 border-gray-300 focus:border-blue-300 focus:ring focus:ring-opacity-50 focus:ring-blue-200 shadow-sm ',
+        naked: 'text-gray-800  bg-white placeholder-gray-300 border-transparent bg-transparent focus:outline-none focus:border-transparent focus:ring-0',
+        success: 'text-gray-800 bg-white placeholder-gray-300 border-green-500 focus:border-green-500 focus:ring focus:ring-opacity-50 focus:ring-green-200 shadow-sm ',
+        danger: 'text-gray-800 bg-white placeholder-gray-300 border-red-500 focus:border-red-500 focus:ring focus:ring-opacity-50 focus:ring-red-200 shadow-sm ',
+        warning: 'text-gray-800 bg-white placeholder-gray-300 border-yellow-500 focus:border-yellow-500 focus:ring focus:ring-opacity-50 focus:ring-yellow-200 shadow-sm ',
+        info: 'text-gray-800 bg-white placeholder-gray-300 border-cyan-500 focus:border-cyan-500 focus:ring focus:ring-opacity-50 focus:ring-cyan-200 shadow-sm ',
     },
     disabled: 'text-gray-400 border-gray-200 focus:border-gray-200 bg-gray-100 shadow-sm ',
     readonly: 'text-gray-800 border-gray-300 focus:border-gray-300 shadow-sm ',
