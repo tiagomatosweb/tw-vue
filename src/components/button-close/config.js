28406098106863a48789c242410a733e47977f28
Tiago Matos <tiago@tiagomatos.com>
2021-10-07 10:37:01 +1100

    Add rounded variation to button close

--- src/components/button-close/config.js
+++ src/components/button-close/config.js
@@ -2,13 +2,13 @@ export default {
     base: 'inline-flex p-1.5 focus:outline-none transition ease-in-out duration-150',
     variants: {
         default: 'text-gray-400 hover:text-gray-500',
-        'default-rounded': 'text-gray-400 hover:text-gray-500 border border-gray-200 rounded-full p-3',
+        'default-rounded': 'text-gray-400 hover:text-gray-500 border border-gray-200 rounded-full bg-white p-3',
         primary: 'text-blue-400 hover:text-blue-500',
-        'primary-rounded': 'text-blue-400 hover:text-blue-500 border border-blue-200 rounded-full p-3',
+        'primary-rounded': 'text-blue-400 hover:text-blue-500 border border-blue-200 rounded-full bg-white p-3',
         danger: 'text-red-400 hover:text-red-500',
-        'danger-rounded': 'text-red-400 hover:text-red-500 border border-red-200 rounded-full p-3',
+        'danger-rounded': 'text-red-400 hover:text-red-500 border border-red-200 rounded-full bg-white p-3',
         success: 'text-green-400 hover:text-green-500',
-        'success-rounded': 'text-green-400 hover:text-green-500 border border-green-200 rounded-full p-3',
+        'success-rounded': 'text-green-400 hover:text-green-500 border border-green-200 rounded-full bg-white p-3',
     },
     sizes: {
         xxs: 'w-3 h-3',
