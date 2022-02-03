0cfa6b04f48b2c6b363baf346fc8787f63232a2e
Tiago Matos <tiago@tiagomatos.com>
2022-02-03 19:23:26 +1100

    Clean eslint file

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,31 +10,6 @@ module.exports = {
         'plugin:vue/essential',
         'eslint:recommended'
     ],
-    // extends: [
-        // 'eslint:recommended',
-        // 'plugin:vue/strongly-recommended',
-        // 'plugin:import/errors',
-        // 'plugin:import/warnings',
-    // ],
     rules: {
-        // 'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-        // 'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-        // 'indent': 'off',
-        // 'comma-dangle': ['error', 'always-multiline'],
-        // 'semi': ['error', 'always'],
-        // 'space-before-function-paren': ['error', 'never'],
-        // 'vue/html-indent': ['error', 4],
-        // 'vue/no-v-html': 'off',
-        // 'vue/script-indent': ['error', 4, {
-        //     baseIndent: 1,
-        //     switchCase: 1,
-        // }],
-        // 'vue/component-name-in-template-casing': ['error', 'PascalCase', {
-        //     'ignores': ['/^c1-/'],
-        //     'registeredComponentsOnly': false,
-        // }],
-        // 'import/no-absolute-path': 'error',
-        // 'import/newline-after-import': 'error',
-        // 'import/no-unresolved': ['error'],
     },
 };
